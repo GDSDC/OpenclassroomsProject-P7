a6224907fe5b80423e4a3d3657ac7682fff809d4
GDSDC <gdc.business.2019@gmail.com>
2022-05-03 20:03:37 +0200

    refactor

--- src/main.py
+++ src/main.py
@@ -6,11 +6,9 @@ from algorithms.optimised_algorithm_v1_3 import get_best_portfolio as ov3_algo
 from algorithms.algorithm_tools import display_portfolio
 
 # CONSTANTS
-ACTIONS_DATA_CSV = 'resources/Informations_sur_les_actions.csv'
-ACTIONS_DATA_CSV_21 = 'resources/Informations_sur_les_actions21.csv'
-
+ACTIONS_DATA_CSV = 'resources/Actions_data/Informations_sur_les_actions.csv'
 
 if __name__ == '__main__':
     print('Voici le meilleur portefeuille d\'investissement !')
-    display_portfolio(sorted(ov3_algo(ACTIONS_DATA_CSV), key=lambda x: x[2],reverse=True))
+    display_portfolio(sorted(bt_algo(ACTIONS_DATA_CSV), key=lambda x: x[2],reverse=True))
 
