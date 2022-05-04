8af6378cb6581fa85169d91d962405e332846925
GDSDC <gdc.business.2019@gmail.com>
2022-05-04 10:10:10 +0200

    feat(performance) : updating o_nlogn_exemple and o_n2_exemple functions for timing comparison

--- src/performance.py
+++ src/performance.py
@@ -9,25 +9,37 @@ import math
 
 # CONSTANTS
 ACTIONS_DATA = 'resources/Actions_data/Performance_data'
-INPUT = [5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+INPUT = [5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24]
 
 
-# Functions
 def o_nlogn_exemple(n):
     """Function to simulate O(nlog(n)) function"""
-    # TODO : code the right function
-    a = []
-    for i in range(n):
-        a.append(i)
+    for i in range(n):  # O(n) iteration
+        # O(log(n) algorithm
+        # Create a list with n elements
+        data = list(range(n))
+        # Choose a value for worst case
+        value = (len(data) - 1) - 1
+        # Init
+        left = 0
+        right = len(data) - 1
+        # Searching Loop
+        while left <= right:
+            middle = (left + right) // 2
+            if value < data[middle]:
+                right = middle - 1
+            elif value > data[middle]:
+                left = middle + 1
+            else:
+                return middle
+        raise ValueError('Value is not in the list')
 
 
 def o_n2_exemple(n):
     """Function to simulate O(n^2) function"""
-    # TODO : code the right function
-    a = []
-    for i in range(n):
-        for j in range(n):
-            a.append(j)
+    if n <= 1:
+        return n
+    return o_n2_exemple(n - 1) + o_n2_exemple(n - 2)
 
 
 def algorithm_time_performance_graph(algorithm, title):
