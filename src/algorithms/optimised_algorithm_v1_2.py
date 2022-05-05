2f933b413e400835ffbb242acd097ec5afa9e11a
GDSDC <gdc.business.2019@gmail.com>
2022-05-05 14:27:17 +0200

    refactor(optimised_algorithm_v1_2) : updating optimised_algorithm_v1_2 function with destructuring

--- src/algorithms/optimised_algorithm_v1_2.py
+++ src/algorithms/optimised_algorithm_v1_2.py
@@ -15,25 +15,27 @@ def optimised_algorithm_v1_2(data):
     actions_data_sorted_under_max_purchase_cost = [action for action in actions_data_sorted if
                                                    get_portfolio_cost([action]) <= MAXIMUM_PURCHASE_COST]
 
-    # Sum action until total cost <= 500 / O(n)
+    # Iteration over actions while total cost <= 500 / O(n)
+    # Init
     best_portfolio.append(actions_data_sorted_under_max_purchase_cost[0])
-    # TODO : write the section below with destructuration so we can understand
-    for i in range(1, len(actions_data_sorted_under_max_purchase_cost)):
-        if (get_portfolio_cost(best_portfolio) + actions_data_sorted_under_max_purchase_cost[i][
-            1] > MAXIMUM_PURCHASE_COST) and (
-                get_portfolio_cost(best_portfolio[:-1]) + actions_data_sorted_under_max_purchase_cost[i][
-                1] <= MAXIMUM_PURCHASE_COST):
-            if best_portfolio[-1][1] * best_portfolio[-1][2] < actions_data_sorted_under_max_purchase_cost[i][1] * \
-                    actions_data_sorted_under_max_purchase_cost[i][2]:
+    # Iteration
+    for action in actions_data_sorted_under_max_purchase_cost[1:]:
+        # Destructuring action
+        _, action_cost, action_benefit_ptc = action
+        # Previous iteration of best_portfolio
+        previous_iteration_portfolio = best_portfolio[:-1]
+        # Las action added in portfolio / action added in previous iteration of best_portfolio
+        _, last_action_added_in_portfolio_cost, last_action_added_in_portfolio_benefit_ptc = best_portfolio[-1]
+
+        if get_portfolio_cost(best_portfolio) == MAXIMUM_PURCHASE_COST:
+            break
+        elif (get_portfolio_cost(best_portfolio) + action_cost > MAXIMUM_PURCHASE_COST) and (
+                get_portfolio_cost(previous_iteration_portfolio) + action_cost <= MAXIMUM_PURCHASE_COST):
+            if last_action_added_in_portfolio_cost * last_action_added_in_portfolio_benefit_ptc < action_cost * action_benefit_ptc:
                 best_portfolio.pop()
-                best_portfolio.append(actions_data_sorted_under_max_purchase_cost[i])
-                if get_portfolio_cost(best_portfolio) == MAXIMUM_PURCHASE_COST:
-                    break
-        elif (get_portfolio_cost(best_portfolio) + actions_data_sorted_under_max_purchase_cost[i][
-                1] < MAXIMUM_PURCHASE_COST):
-            best_portfolio.append(actions_data_sorted_under_max_purchase_cost[i])
-            if get_portfolio_cost(best_portfolio) == MAXIMUM_PURCHASE_COST:
-                break
+                best_portfolio.append(action)
+        elif get_portfolio_cost(best_portfolio) + action_cost < MAXIMUM_PURCHASE_COST:
+            best_portfolio.append(action)
         else:
             continue
 
