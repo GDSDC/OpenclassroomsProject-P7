afa2a65a7395d36d6eac0d3fb8c016a08539e8b1
gabrieldacosta <gdc.business.2019@gmail.com>
2022-05-09 19:18:39 +0200

    refactor(imports) : importing optimised_algorithm_dynamic

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from algorithms.optimised_algorithm_v1_2 import optimised_algorithm_v1_2
 from algorithms.optimised_algorithm_v1_3 import optimised_algorithm_v1_3
 from algorithms.optimised_algorithm_v1_4 import optimised_algorithm_v1_4
 from algorithms.optimised_algorithm_v1_5 import optimised_algorithm_v1_5
+from algorithms.optimised_algorithm_dynamic import optimised_algorithm_dynamic
 from algorithms.algorithm_tools import display_best_portfolio, get_csv_data
 
 # CONSTANTS
@@ -13,5 +14,5 @@ ACTIONS_DATA = get_csv_data(ACTIONS_DATA_CSV)
 
 if __name__ == '__main__':
     print('')
-    display_best_portfolio(algorithm=optimised_algorithm_v1_4, data=ACTIONS_DATA)
+    display_best_portfolio(algorithm=optimised_algorithm_dynamic, data=ACTIONS_DATA)
     print('')
