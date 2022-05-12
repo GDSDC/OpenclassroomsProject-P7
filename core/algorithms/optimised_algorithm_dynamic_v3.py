64de3937119db75916f77de4009a6c0737c42f1c
gabrieldacosta <gdc.business.2019@gmail.com>
2022-05-12 16:41:24 +0200

    feat(optimised algorithm) : creating v2 and v3 of optimised_algorithm_dynamic.py

---
+++ core/algorithms/optimised_algorithm_dynamic_v3.py
@@ -0,0 +1,65 @@
+# Imports
+from typing import List
+from core.model import MAXIMUM_PURCHASE_COST, Portfolio, Action
+from math import ceil as rounded_upp
+
+
+def optimised_algorithm_dynamic_v3(actions: List[Action], max_cost: int = MAXIMUM_PURCHASE_COST,
+                                   step: int = 5) -> Portfolio:
+    """
+    Function that returns the best portfolio - dynamic algorithm
+    cf https://fr.wikipedia.org/wiki/Probl%C3%A8me_du_sac_%C3%A0_dos paragraphe Resolution exacte
+    """
+
+    # Better Time Performance because of calculating on step interval, and reducing optimised_space
+
+    # Init -> O(1)
+    actions_count = len(actions)
+    optimised_space = []
+    max_cost_idx = (max_cost // step) + 1
+
+    # Iteration -> O(n^2)
+    for nb_actions in range(actions_count + 1):
+        # nb_actions -> number of actions in portfolio
+        # Create an empty 'row' in optimised_space
+        optimised_space.append([])
+        for portfolio_funds_idx in range(max_cost_idx + 1):
+            portfolio_funds = portfolio_funds_idx * step
+            # portfolio_funds -> funds value for portfolio to buy in action
+            # Create an empty 'column' in optimised_space[nb_actions]
+            optimised_space[nb_actions].append([])
+
+            # Initialization : empty value for init
+            if nb_actions == 0 or portfolio_funds_idx == 0:
+                pass
+            else:
+                action = actions[nb_actions - 1]  # current action index = nb_actions - 1
+                # because actions[0] correspond to nb_actions = 1
+                last_best_portfolio = optimised_space[nb_actions - 1][portfolio_funds_idx]
+                last_best_portfolio_without_action_funds = optimised_space[nb_actions - 1][
+                    int((portfolio_funds - rounded_upp(action.cost / step) * step) / step)]
+
+                if action.cost <= portfolio_funds:
+                    value_after_two_years_portfolio_WITH_action = action.value_after_two_years + Portfolio(
+                        actions=last_best_portfolio_without_action_funds).value_after_two_years
+                    value_after_two_years_portfolio_WITHOUT_action = Portfolio(
+                        actions=last_best_portfolio).value_after_two_years
+
+                    if value_after_two_years_portfolio_WITH_action > value_after_two_years_portfolio_WITHOUT_action:
+                        # Create new_best_portfolio with action in there
+                        new_best_portfolio = []
+                        new_best_portfolio.extend(last_best_portfolio_without_action_funds)
+                        new_best_portfolio.extend([action])
+                        optimised_space[nb_actions][portfolio_funds_idx].extend(new_best_portfolio)
+
+                    else:
+                        # new_best_portfolio = last_best_portfolio
+                        optimised_space[nb_actions][portfolio_funds_idx].extend(last_best_portfolio)
+
+                else:
+                    # new_best_portfolio = last_best_portfolio
+                    optimised_space[nb_actions][portfolio_funds_idx].extend(last_best_portfolio)
+
+    return Portfolio(actions=optimised_space[actions_count][max_cost_idx - 1])
+
+    # Overall Complexity = O(n^2)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import List
 from core.algorithms.bruteforce import bruteforce_algorithm
 from core.algorithms.optimised_algorithm_greedy import optimised_algorithm_greedy
 from core.algorithms.optimised_algorithm_dynamic import optimised_algorithm_dynamic
+from core.algorithms.optimised_algorithm_dynamic_v2 import optimised_algorithm_dynamic_v2
+from core.algorithms.optimised_algorithm_dynamic_v3 import optimised_algorithm_dynamic_v3
 from core.algorithms.algorithm_tools import display_portfolio, get_csv_data
 from core.model import Action
 import sys
@@ -15,6 +17,8 @@ ALGORITHM_PROVIDER = {
     'bf_algo': bruteforce_algorithm,
     'greedy_algo': optimised_algorithm_greedy,
     'dyn_algo': optimised_algorithm_dynamic,
+    'dyn_algo_v2' : optimised_algorithm_dynamic_v2,
+    'dyn_algo_v3': optimised_algorithm_dynamic_v3
 }
 
 # DISPLAY MAIN FUNCTION
