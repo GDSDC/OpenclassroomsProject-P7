5e2efa9f77939a7b581fa66399a7d56630e7ac75
gabrieldacosta <gdc.business.2019@gmail.com>
2022-05-19 10:47:29 +0200

    refactor(dyn_algo_v2) : removing removing dyn_algo_v2 because we only keep dyn_algo_v3 for better performances

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from typing import List
 from core.algorithms.bruteforce import bruteforce_algorithm
 from core.algorithms.optimised_algorithm_greedy import optimised_algorithm_greedy
 from core.algorithms.optimised_algorithm_dynamic import optimised_algorithm_dynamic
-from core.algorithms.optimised_algorithm_dynamic_v2 import optimised_algorithm_dynamic_v2
 from core.algorithms.optimised_algorithm_dynamic_v3 import optimised_algorithm_dynamic_v3
 from core.algorithms.algorithm_tools import display_portfolio, get_csv_data
 from core.model import Action
@@ -17,7 +16,6 @@ ALGORITHM_PROVIDER = {
     'bf_algo': bruteforce_algorithm,
     'greedy_algo': optimised_algorithm_greedy,
     'dyn_algo': optimised_algorithm_dynamic,
-    'dyn_algo_v2' : optimised_algorithm_dynamic_v2,
     'dyn_algo_v3': optimised_algorithm_dynamic_v3
 }
 
