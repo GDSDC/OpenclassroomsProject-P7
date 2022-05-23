7b1445c5aa26bc7ecd14c083c87c4dd8d78272fb
gabrieldacosta <gdc.business.2019@gmail.com>
2022-05-23 11:39:56 +0200

    style

--- core/algorithms/algorithm_tools.py
+++ core/algorithms/algorithm_tools.py
@@ -21,7 +21,7 @@ def timer_func(func):
         t1 = time()
         result = func(*args, **kwargs)
         t2 = time()
-        print(f'Function {func.__name__!r} executed in {(t2 - t1):.4f}s')
+        print(f'Function {func.__name__!r} executed in {round(t2 - t1,2)}s')
         return result
 
     return wrap_func
